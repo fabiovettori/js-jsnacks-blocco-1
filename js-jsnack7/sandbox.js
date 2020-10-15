9bdf17c2238e9090d4993ac9d634acde715f91c1
Fabio Vettori <fabio_vettori@hotmail.it>
2020-10-15 23:42:04 +0200

    js-jsnack7

--- js-jsnack7/sandbox.js
+++ js-jsnack7/sandbox.js
@@ -1,2 +1,16 @@
 // JSnack 7.
 // Calcola la somma e la media dei numeri da 1 a 10.
+
+var array = [];
+for (var i = 1; i <= 10; i++) {;
+    var counter1 = i;
+    array.push(counter1);
+}
+
+var counter2 = 0;
+for (var i = 0; i < array.length; i++) {
+    counter2 = counter2 + array[i];
+}
+
+console.log('summa = ' + counter2);
+console.log('average = ' + counter2/10);
