d9e2f2f8553cec34d5cdd131b8ee39b9f23b08f6
Fabio Vettori <fabio_vettori@hotmail.it>
2020-10-15 18:09:20 +0200

    js-jsnack1

--- js-jsnack1/sandbox.js
+++ js-jsnack1/sandbox.js
@@ -1,2 +1,13 @@
 // JSnack 1.
 // L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
+
+var input1 = parseInt(prompt('Please insert the first number'));
+var input2 = parseInt(prompt('Please insert the second number'));
+
+if (input1 > input2) {
+    console.log(input1);
+} else if (input2 > input1) {
+    console.log(input2);
+} else {
+    console.log('input1 = input2');
+}
