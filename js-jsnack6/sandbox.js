00af6ac4f78a2d4d507c00142879a3789bce0ff9
Fabio Vettori <fabio_vettori@hotmail.it>
2020-10-15 22:31:29 +0200

    js-jsnack6

--- js-jsnack6/sandbox.js
+++ js-jsnack6/sandbox.js
@@ -1,2 +1,9 @@
 // JSnack 6.
 // Stampa le potenze di 2 senza superare il numero 1000.
+
+var check = 0;
+for (var i = 0; Math.pow(2, i) <= 1000; i++) {
+    var output = Math.pow(2, i);
+    check = output;
+    console.log(output);
+}
